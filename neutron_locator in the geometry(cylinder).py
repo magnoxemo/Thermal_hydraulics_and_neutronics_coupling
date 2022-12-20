32273df1a40e58e97a07f1496d484f3a0f5b801f
Ibne Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2022-12-20 10:32:13 +0600

    Create neutron_locator in the geometry(cylinder).py

---
+++ neutron_locator in the geometry(cylinder).py
@@ -0,0 +1,23 @@
+def neutron_locator(x,y,z):
+  
+	def search(x,y):
+    #will write it's details when my exams are over 
+    pass 
+  
+  def material_id(location_id):
+     #will write it's details when my exams are over 
+    pass 
+ 	
+	x=x
+	y=y
+	z=z
+  
+	if np.sqrt(x**2+y**2)>core_dim:
+		neutron_life=False
+	elif z>core_dim:
+		neutron_life=False
+		break
+	else 
+		location_id=search(x,y)
+		material_id=material_search(location_id)
+    return material_id
