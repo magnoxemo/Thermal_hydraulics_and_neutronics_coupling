3d2a388fb837173c8ca36b154ce03d4cea44c39b
Ibne Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2023-01-25 23:48:08 +0600

    Update dopplerbrdr.py
    Main problem is in the Ak+Bk*E term in the integration formula

--- src/dopplerbrdr.py
+++ src/dopplerbrdr.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#----------------------importing data file----------------------
+#----------------------importing data file & constant defining ----------------------
 data=pd.read_csv('U-235 fission.csv')
 energy=np.array(data['incident energy'])
 cross_section=np.array(data['cross section'])
@@ -20,26 +20,47 @@ alpha=(M/m)/(k*Temp2)
 
 y=alpha*energy
 Er=RelE(energy)         #relative energy calculation
+
+
 """------------------------------interpolation------------------------------------"""
 
 Ak=np.zeros(len(x)-1)       #constant term in linear interpolation 
 Bk=np.zeros(len(x)-1)       #term which is propotional to E
 
+
+
 # calculation loop for interpolation 
 for i in range(len(energy)-1):
     Ak[i]=(energy[i+1]*cross_section[i]-energy[i]*cross_section[i+1])/(energy[i+1]-energy[i])
     Bk[i]=(cross_section[i+1]-cross_section[i])/(energy[i+1]-energy[i])
+
+
     
 """-----------------------------integration part-----------------------------------"""
 """there will be two loops.one to count the integral and one to count the data points"""
+
+def RangeFinder(E):
+
+"""this function will take E parameter and return the coressponding index value of the 
+linear intepolator's coefficient 
+				
+				Ak[i]----> the constant term
+				Bk[i]----> the relative term 
+first there will be a linear search in the energy[] list then return the index of that range in energy  """
+ 
 
 brdr_sigma=np.zeros(len(energy)-1)
 for i in range(len(y)-1):
     intergral=0
     for j in range(len(energy)-1):
         delEr=abs(Er[j+1]-Er[j])
+        
+        #interpolation's problem fixed 
+        
         intergral=intergral+np.sqrt(Er[j])*(Ak[j]+Bk[j]*Er[j])*delEr*(np.exp(-alpha*(energy[i]-Er[j])**2)-np.exp(-alpha*(energy[i]+Er[j])**2))
+        
     brdr_sigma[i]=0.5*np.sqrt(alpha/(np.pi*energy[i]))*intergral
+    
     
     
 #---------------------------data visualization--------------------------
