a2d1239daaf2f06328bd4d05ca0d709432fb5444
Ibne Walid Ahammed <69362074+magnoxemo@users.noreply.github.com>
2023-02-17 18:47:48 +0600

    Update materials.py

--- src/materials.py
+++ src/materials.py
@@ -1,7 +1,19 @@
+""" 
+
+A full core Monte Carlo for Nuclear space rocket analysis (material section)-on development 
+
+copyright:
+
+Ebny Walid Ahammed                   
+Dept. of Nuclear Engineering
+University of Dhaka 
+
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd 
 import scipy as sp
+from tqdm import tqdm
 from scipy.interpolate import interp1d
 from scipy.intergrate import trapz
 
@@ -12,13 +24,13 @@ class material:
 		M=Atomic_mass										#atomic mass of the element 
 		density=density									#atomic density
 		NA=6.023*10**23
-		if enrichment==None									#avogrado constant 
+		if enrichment==None:									#avogrado constant 
 			enrichment=1	
-		else
+		else:
 			enrichment=enrichment								#enrichment 
-		if temp==None
+		if temp==None:
 			temp=0
-		else  
+		else  :
 			temp=temp							#temp needs to perform doppler boardening 
 		ReactionType=ReactionType								#types of the reaction will happen
 		energy,sigma={}							                #cross section dictionary  
@@ -40,73 +52,76 @@ class material:
 		try:	
 			for i in range(Type_Reaction):
 
-				path[i]=str(cross section data /)+str(self.M)+str(self.ReactionType[i])+str(.csv)
+				path[i]=str(cross section data/)+str(self.M)+str(self.ReactionType[i])+str(.csv)
 				xscetion[i]=pd.readcsv(path)
-				engergy[str(self.ReactionType[i])]=xsection[i]["Incident energy"]
-				sigma[str(self.ReactionType[i])]=xsection[i]["Cross section"]
+				engergy[str(self.ReactionType[i])]=np.array(xsection[i]["Incident energy"])
+				sigma[str(self.ReactionType[i])]=np.array(xsection[i]["Cross section"])
 				
 			return energy,sigma
 		except:
 			raise data_not_found_in_the_bank_Error(str(self.ReactionType[i])," not found ")
 	def Dopplerdrbr(self):
-	    	energy1=np.array(self.)
-	    	cross_section=np.array()
-
-	    	m=1                      							      #projectile mass-----gram atom 
-	    	M=self.M                     							      #target mass    -----gram atom
-	    	k=8.617*10**-5            							      #boltzman const -----eV 
-	    	Temp2=self.temp                   						      #target temp---------Kelvin scale 
-	    	Temp1=0
-	    	alpha=(M/m)/(k*(Temp2-Temp1))   
-
-	    	"""------------------------------interpolation------------------------------------"""
-
-	    	sigma_a=interp1d(energy1,cross_section)
-
-	    	#-----------resonance regionsplit----------
-
-	    	def ResonaceSplit(energy,cross_section):
-			LowRE=10**-1
-			HighRE=10**4
-			for i in (range(len(energy))):
-		    		if energy[i]>LowRE:
-		        	break
-			for j in range(i,len(energy)):
-		    	if energy[j]>HighRE:
-		        	break
+		for i in ReactionType:
 		
-			ResonaceEnergy=energy1[i:j]
-			Resonacecross=cross_section[i:j]
+			if self.temp!=0:
+			    	energy1=np.array(self.energy[i])
+			    	cross_section=np.array(self.sigma[i])
 
-			return ResonaceEnergy,Resonacecross,i,j
+			    	m=1                      							      #projectile mass-----gram atom 
+			    	M=self.M                     							      #target mass    -----gram atom
+			    	k=8.617*10**-5            							      #boltzman const -----eV 
+			    	Temp2=self.temp                   						      #target temp---------Kelvin scale 
+			    	Temp1=0
+			    	alpha=(M/m)/(k*(Temp2-Temp1))   
 
-	    	energy,ResonaceSigma,st,ed=ResonaceSplit(energy1,cross_section)
-	    	
+			    	"""------------------------------interpolation------------------------------------"""
 
-	    #-------------integration calculation --------------------
-	    	E_r=np.zeros(len(energy1))
-	   	E_min=np.min(energy)
-	    	E_max=np.max(energy)
-	    	step=10**6
+			    	sigma_a=interp1d(energy1,cross_section)
 
-	    	Er=np.linspace(E_min,E_max,step)
-	    	dx=(E_max-E_min)/step
+			    	#-----------resonance regionsplit----------
 
-	    #----------------F(x) calculation in the integration loop--------------
-	    
-	    	def CalFx(x):
+			    	def ResonaceSplit(energy,cross_section):
+					LowRE=10**-1
+					HighRE=10**4
+					for i in (range(len(energy))):
+				    		if energy[i]>LowRE:
+						break
+					for j in range(i,len(energy)):
+				    	if energy[j]>HighRE:
+						break
+				
+					ResonaceEnergy=energy1[i:j]
+					Resonacecross=cross_section[i:j]
 
-			E_r=np.sqrt(Er)*sigma_a(Er)*(np.exp(-alpha*(np.sqrt(x)-np.sqrt(Er))**2)-np.exp(-alpha*(np.sqrt(x)+np.sqrt(Er))**2))
+					return ResonaceEnergy,Resonacecross,i,j
 
-			return E_r
-	    	sigma=np.zeros(len(cross_section))
+			    	energy,ResonaceSigma,st,ed=ResonaceSplit(energy1,cross_section)
+			    	
 
-	    	for i in tqdm(range(len(energy))):
-			sigma[i]=1/energy[i]*0.5*np.sqrt(alpha/(np.pi))*np.trapz(CalFx(energy[i]),x=Er,dx=dx)
-		
-		cross_section[st:ed-1]=sigma[:len(energy)-1]
-		
-		return energy1,cross_section
+			    #-------------integration calculation --------------------
+			    	E_r=np.zeros(len(energy1))
+			   	E_min=np.min(energy)
+			    	E_max=np.max(energy)
+			    	step=10**6
+
+			    	Er=np.linspace(E_min,E_max,step)
+			    	dx=(E_max-E_min)/step
+
+			    #----------------F(x) calculation in the integration loop--------------
+			    
+			    	def CalFx(x):
+
+					E_r=np.sqrt(Er)*sigma_a(Er)*(np.exp(-alpha*(np.sqrt(x)-np.sqrt(Er))**2)-np.exp(-alpha*(np.sqrt(x)+np.sqrt(Er))**2))
+
+					return E_r
+			    	sigma=np.zeros(len(cross_section))
+
+			    	for i in tqdm(range(len(energy))):
+					sigma[i]=1/energy[i]*0.5*np.sqrt(alpha/(np.pi))*np.trapz(CalFx(energy[i]),x=Er,dx=dx)
+				
+				cross_section[st:ed-1]=sigma[:len(energy)-1]
+				
+				return energy1,cross_section
 		
 		
 	def MacroScopicCross(self):
